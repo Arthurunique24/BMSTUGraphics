c435e70670e977b5670fa1f191ece0b5799fd0e8
Arthur Sarkisyan <arthurunique24@gmail.com>
2017-11-29 17:29:48 +0300

    Homework with Bresenham's circle algorithm

---
+++ HWBresenham/canvas.js
@@ -0,0 +1,114 @@
+let canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+
+let maxRadius = 70;
+let countOfCircles = 200;
+let colorArray = ["#99B898", "#FECEA8", "#FF847C", "#E84A5F"];
+
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    init();
+});
+
+function BrosenhamCircle(X1, Y1, R, circleColor) {
+    let x = 0;
+    let y = R;
+    let delta = 1 - 2 * R;
+    let error = 0;
+
+    while (y >= 0) {
+        c.fillStyle = circleColor;
+        c.fillRect(X1 + x, Y1 + y, 1, 1);
+        c.fillRect(X1 + x, Y1 - y, 1, 1);
+        c.fillRect(X1 - x, Y1 + y, 1, 1);
+        c.fillRect(X1 - x, Y1 - y, 1, 1);
+
+        error = 2 * (delta + y) - 1;
+        if ((delta < 0) && (error <= 0)) {
+            delta += 2 * ++x + 1;
+            continue;
+        }
+
+        error = 2 * (delta - x) - 1;
+        if ((delta > 0) && (error > 0)) {
+            delta += 1 - 2 * --y;
+            continue;
+        }
+        x++;
+        delta += 2 * (x - y);
+        y--;
+    }
+}
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.minRadius = radius;
+    this.colorArray = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+    this.draw = function () {
+        // c.beginPath();
+        // c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+
+        //If you want epileptic effect
+        // //c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+        // c.fillStyle = this.colorArray;
+        // c.fill();
+        BrosenhamCircle(this.x, this.y, radius, this.colorArray);
+    };
+
+    this.update = function () {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = - this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+let circleArray = [];
+
+function init() {
+    circleArray = [];
+
+    for (let i = 0; i < countOfCircles; i++) {
+        //let radius = 30;
+        let radius = Math.random() * 30 + 5;
+        let x = Math.random() * (innerWidth - radius * 2) + radius;
+        let y = Math.random() * (innerHeight - radius * 2) + radius;
+        let dx = (Math.random() - 0.5);
+        let dy = (Math.random() - 0.5);
+
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (let i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+
+}
+
+init();
+animate();
